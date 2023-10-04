6522ae0543ab7641bb875e8413a1da632f7562ab
Jonas Escobar <jonasky17@gmail.com>
2023-10-04 19:36:12 +0800

    need fs

--- src/js/bs.js
+++ src/js/bs.js
@@ -1,40 +1,101 @@
+// import * as fs from 'fs';
+var json_data = [];
 document.addEventListener("DOMContentLoaded", () => {
     const get_data = localStorage.getItem("billing_data");
     const data = JSON.parse(get_data);
     console.log(data.items[0].description);
 
     var cName = document.getElementById("customer_name");
-    if(data.customer_name != ""){
+    if (data.customer_name != "") {
         cName.innerHTML = data.customer_name;
-    }else{
+    } else {
         cName.innerHTML = "-";
     }
 
     var cAddress = document.getElementById("customer_address");
-    if(data.customer_address != ""){
+    if (data.customer_address != "") {
         cAddress.innerHTML = data.customer_address;
-    }else{
+    } else {
         cAddress.innerHTML = "-";
     }
 
     var cEmail = document.getElementById("customer_email");
-    if(data.customer_email != ""){
+    if (data.customer_email != "") {
         cEmail.innerHTML = data.customer_email;
-    }else{
+    } else {
         cEmail.innerHTML = "-";
     }
 
     var cContact = document.getElementById("customer_contact");
-    if(data.customer_contact != ""){
+    if (data.customer_contact != "") {
         cContact.innerHTML = data.customer_contact;
-    }else{
+    } else {
         cContact.innerHTML = "-";
     }
 
     var bDate = document.getElementById("billing_date");
-    if(data.billing_date != ""){
+    if (data.billing_date != "") {
         bDate.innerHTML = data.billing_date;
-    }else{
+    } else {
         bDate.innerHTML = "-";
     }
-  });
+
+    var compute_total = 0;
+
+    for (var i = 0; i < data.items.length; i++) {
+        var new_tr = document.createElement("tr");
+        var new_td_desc = document.createElement("td");
+        new_td_desc.classList.add("text");
+        new_td_desc.innerHTML = data.items[i].description;
+        new_tr.appendChild(new_td_desc);
+
+        var new_td_price = document.createElement("td");
+        new_td_price.classList.add("currency");
+        new_td_price.classList.add("price");
+        new_td_price.innerHTML = data.items[i].price;
+        new_tr.appendChild(new_td_price);
+
+        var new_td_qty = document.createElement("td");
+        new_td_qty.classList.add("currency");
+        new_td_qty.classList.add("qty");
+        new_td_qty.innerHTML = data.items[i].qty;
+        new_tr.appendChild(new_td_qty);
+
+        var new_td_total = document.createElement("td");
+        new_td_total.classList.add("currency");
+        new_td_total.classList.add("total");
+        new_td_total.innerHTML = data.items[i].total;
+        new_tr.appendChild(new_td_total);
+
+        var get_body = document.getElementById("bs_items");
+        get_body.appendChild(new_tr);
+
+        compute_total += Number(data.items[i].total);
+        var get_grand_total = document.getElementById("grand_total");
+        get_grand_total.innerHTML = compute_total;
+        console.log(data.items[i].total)
+        console.log(compute_total);
+    }
+
+    var MD5 = function (d) { var r = M(V(Y(X(d), 8 * d.length))); return r.toLowerCase() }; function M(d) { for (var _, m = "0123456789ABCDEF", f = "", r = 0; r < d.length; r++)_ = d.charCodeAt(r), f += m.charAt(_ >>> 4 & 15) + m.charAt(15 & _); return f } function X(d) { for (var _ = Array(d.length >> 2), m = 0; m < _.length; m++)_[m] = 0; for (m = 0; m < 8 * d.length; m += 8)_[m >> 5] |= (255 & d.charCodeAt(m / 8)) << m % 32; return _ } function V(d) { for (var _ = "", m = 0; m < 32 * d.length; m += 8)_ += String.fromCharCode(d[m >> 5] >>> m % 32 & 255); return _ } function Y(d, _) { d[_ >> 5] |= 128 << _ % 32, d[14 + (_ + 64 >>> 9 << 4)] = _; for (var m = 1732584193, f = -271733879, r = -1732584194, i = 271733878, n = 0; n < d.length; n += 16) { var h = m, t = f, g = r, e = i; f = md5_ii(f = md5_ii(f = md5_ii(f = md5_ii(f = md5_hh(f = md5_hh(f = md5_hh(f = md5_hh(f = md5_gg(f = md5_gg(f = md5_gg(f = md5_gg(f = md5_ff(f = md5_ff(f = md5_ff(f = md5_ff(f, r = md5_ff(r, i = md5_ff(i, m = md5_ff(m, f, r, i, d[n + 0], 7, -680876936), f, r, d[n + 1], 12, -389564586), m, f, d[n + 2], 17, 606105819), i, m, d[n + 3], 22, -1044525330), r = md5_ff(r, i = md5_ff(i, m = md5_ff(m, f, r, i, d[n + 4], 7, -176418897), f, r, d[n + 5], 12, 1200080426), m, f, d[n + 6], 17, -1473231341), i, m, d[n + 7], 22, -45705983), r = md5_ff(r, i = md5_ff(i, m = md5_ff(m, f, r, i, d[n + 8], 7, 1770035416), f, r, d[n + 9], 12, -1958414417), m, f, d[n + 10], 17, -42063), i, m, d[n + 11], 22, -1990404162), r = md5_ff(r, i = md5_ff(i, m = md5_ff(m, f, r, i, d[n + 12], 7, 1804603682), f, r, d[n + 13], 12, -40341101), m, f, d[n + 14], 17, -1502002290), i, m, d[n + 15], 22, 1236535329), r = md5_gg(r, i = md5_gg(i, m = md5_gg(m, f, r, i, d[n + 1], 5, -165796510), f, r, d[n + 6], 9, -1069501632), m, f, d[n + 11], 14, 643717713), i, m, d[n + 0], 20, -373897302), r = md5_gg(r, i = md5_gg(i, m = md5_gg(m, f, r, i, d[n + 5], 5, -701558691), f, r, d[n + 10], 9, 38016083), m, f, d[n + 15], 14, -660478335), i, m, d[n + 4], 20, -405537848), r = md5_gg(r, i = md5_gg(i, m = md5_gg(m, f, r, i, d[n + 9], 5, 568446438), f, r, d[n + 14], 9, -1019803690), m, f, d[n + 3], 14, -187363961), i, m, d[n + 8], 20, 1163531501), r = md5_gg(r, i = md5_gg(i, m = md5_gg(m, f, r, i, d[n + 13], 5, -1444681467), f, r, d[n + 2], 9, -51403784), m, f, d[n + 7], 14, 1735328473), i, m, d[n + 12], 20, -1926607734), r = md5_hh(r, i = md5_hh(i, m = md5_hh(m, f, r, i, d[n + 5], 4, -378558), f, r, d[n + 8], 11, -2022574463), m, f, d[n + 11], 16, 1839030562), i, m, d[n + 14], 23, -35309556), r = md5_hh(r, i = md5_hh(i, m = md5_hh(m, f, r, i, d[n + 1], 4, -1530992060), f, r, d[n + 4], 11, 1272893353), m, f, d[n + 7], 16, -155497632), i, m, d[n + 10], 23, -1094730640), r = md5_hh(r, i = md5_hh(i, m = md5_hh(m, f, r, i, d[n + 13], 4, 681279174), f, r, d[n + 0], 11, -358537222), m, f, d[n + 3], 16, -722521979), i, m, d[n + 6], 23, 76029189), r = md5_hh(r, i = md5_hh(i, m = md5_hh(m, f, r, i, d[n + 9], 4, -640364487), f, r, d[n + 12], 11, -421815835), m, f, d[n + 15], 16, 530742520), i, m, d[n + 2], 23, -995338651), r = md5_ii(r, i = md5_ii(i, m = md5_ii(m, f, r, i, d[n + 0], 6, -198630844), f, r, d[n + 7], 10, 1126891415), m, f, d[n + 14], 15, -1416354905), i, m, d[n + 5], 21, -57434055), r = md5_ii(r, i = md5_ii(i, m = md5_ii(m, f, r, i, d[n + 12], 6, 1700485571), f, r, d[n + 3], 10, -1894986606), m, f, d[n + 10], 15, -1051523), i, m, d[n + 1], 21, -2054922799), r = md5_ii(r, i = md5_ii(i, m = md5_ii(m, f, r, i, d[n + 8], 6, 1873313359), f, r, d[n + 15], 10, -30611744), m, f, d[n + 6], 15, -1560198380), i, m, d[n + 13], 21, 1309151649), r = md5_ii(r, i = md5_ii(i, m = md5_ii(m, f, r, i, d[n + 4], 6, -145523070), f, r, d[n + 11], 10, -1120210379), m, f, d[n + 2], 15, 718787259), i, m, d[n + 9], 21, -343485551), m = safe_add(m, h), f = safe_add(f, t), r = safe_add(r, g), i = safe_add(i, e) } return Array(m, f, r, i) } function md5_cmn(d, _, m, f, r, i) { return safe_add(bit_rol(safe_add(safe_add(_, d), safe_add(f, i)), r), m) } function md5_ff(d, _, m, f, r, i, n) { return md5_cmn(_ & m | ~_ & f, d, _, r, i, n) } function md5_gg(d, _, m, f, r, i, n) { return md5_cmn(_ & f | m & ~f, d, _, r, i, n) } function md5_hh(d, _, m, f, r, i, n) { return md5_cmn(_ ^ m ^ f, d, _, r, i, n) } function md5_ii(d, _, m, f, r, i, n) { return md5_cmn(m ^ (_ | ~f), d, _, r, i, n) } function safe_add(d, _) { var m = (65535 & d) + (65535 & _); return (d >> 16) + (_ >> 16) + (m >> 16) << 16 | 65535 & m } function bit_rol(d, _) { return d << _ | d >>> 32 - _ }
+
+    var bs_id = "tabs_bs" + data.customer_name + Math.floor(Date.now() / 100);
+    var get_id = document.getElementById("billing_id");
+    get_id.innerHTML = MD5(bs_id);
+
+    data["id"] = MD5(bs_id);
+    json_data = data;
+});
+
+function save() {
+    let yourDate = new Date()
+    let final_date = yourDate.toISOString().split('T')[0]
+    // const fs = require("fs");
+    fs.writeFile(json_data.customer_name+final_date+".json",json_data,(error)=>{
+        if(error){
+            console.log(error);
+            throw error;
+        }
+    });
+    console.log(final_date);
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     var getBD = document.getElementById("billing_date");
-    var d =  new Date();
+    var d = new Date();
     console.log(d);
     getBD.value = formatDate(new Date());
 });
@@ -13,34 +13,43 @@ document.getElementById("btn_generate").addEventListener("click", function (even
     event.preventDefault();
 });
 
-function addItem(){
+function addItem() {
     var getIndex = document.getElementsByName("description[]");
     // console.log(getIndex.length);
     const newTR = document.createElement("tr");
     const newTDDesc = document.createElement("td");
     const newInputDesc = document.createElement("input");
     newInputDesc.classList.add("tbl-input");
-    newInputDesc.setAttribute("name","description[]");
+    newInputDesc.setAttribute("name", "description[]");
     newTDDesc.appendChild(newInputDesc);
 
     const newTDPrice = document.createElement("td");
     const newInputPrice = document.createElement("input");
     newInputPrice.classList.add("tbl-input");
-    newInputPrice.setAttribute("name","price[]");
+    newInputPrice.classList.add("number");
+    newInputPrice.classList.add("price");
+    newInputPrice.setAttribute("name", "price[]");
+    newInputPrice.setAttribute("onchange", "computePrice(this)");
     newTDPrice.appendChild(newInputPrice);
-    
+
     const newTDQty = document.createElement("td");
     const newInputQty = document.createElement("input");
     newInputQty.classList.add("tbl-input");
-    newInputQty.setAttribute("name","qty[]");
+    newInputQty.classList.add("number");
+    newInputQty.classList.add("qty");
+    newInputQty.setAttribute("name", "qty[]");
+    newInputQty.setAttribute("onchange", "computeQty(this)");
     newTDQty.appendChild(newInputQty);
 
     const newTDTotal = document.createElement("td");
     const newInputTotal = document.createElement("input");
     newInputTotal.classList.add("tbl-input");
-    newInputTotal.setAttribute("name","total[]");
+    newInputTotal.classList.add("number");
+    newInputTotal.classList.add("total");
+    newInputTotal.setAttribute("name", "total[]");
+    newInputTotal.setAttribute("onchange","getTotal()");
     newTDTotal.appendChild(newInputTotal);
-    
+
     newTR.appendChild(newTDDesc);
     newTR.appendChild(newTDPrice);
     newTR.appendChild(newTDQty);
@@ -51,12 +60,12 @@ function addItem(){
     getItems.appendChild(newTR);
 }
 
-function generate(){
+function generate() {
     var elements = document.getElementById("form_generate").elements;
-    var obj ={};
-    for(var i = 0 ; i < elements.length ; i++){
+    var obj = {};
+    for (var i = 0; i < elements.length; i++) {
         var item = elements.item(i);
-        if(item.name === "customer_name" ||item.name === "customer_address" || item.name === "customer_email" || item.name === "customer_contact" || item.name === "billing_date" ){
+        if (item.name === "customer_name" || item.name === "customer_address" || item.name === "customer_email" || item.name === "customer_contact" || item.name === "billing_date") {
             obj[item.name] = item.value;
         }
     }
@@ -65,31 +74,64 @@ function generate(){
     var get_qty = document.getElementsByName("qty[]");
     var get_total = document.getElementsByName("total[]");
     var items = [];
-    for(var i = 0; i < get_description.length; i++){
+    for (var i = 0; i < get_description.length; i++) {
         var item = {};
-        console.log(get_description[i].value,get_price[i].value,get_qty[i].value,get_total[i].value);
+        console.log(get_description[i].value, get_price[i].value, get_qty[i].value, get_total[i].value);
         item["description"] = get_description[i].value;
         item["price"] = get_price[i].value;
         item["qty"] = get_qty[i].value;
         item["total"] = get_total[i].value;
         items.push(item);
     }
-    console.log(items);    
+    console.log(items);
     obj["items"] = items;
     localStorage.removeItem('billing_data');
-    console.log("data",obj);
-    localStorage.setItem("billing_data", JSON.stringify(obj));   
+    console.log("data", obj);
+    localStorage.setItem("billing_data", JSON.stringify(obj));
     window.open("http://localhost/billing/src/template/bs.html");
 }
 
 function formatDate(date = new Date()) {
-    const year = date.toLocaleString('default', {year: 'numeric'});
+    const year = date.toLocaleString('default', { year: 'numeric' });
     const month = date.toLocaleString('default', {
-      month: '2-digit',
+        month: '2-digit',
     });
-    const day = date.toLocaleString('default', {day: '2-digit'});
-  
+    const day = date.toLocaleString('default', { day: '2-digit' });
+
     return [year, month, day].join('-');
-  }
+}
 
+function getTotal(){
+    var get_totals = document.getElementsByName("total[]");
+    console.log("totals", get_totals);
+    var total = 0;
+    for(var i = 0; i < get_totals.length; i++){
+        total += Number(get_totals[i].value);
+    }
+    var getElement = document.getElementById("grand_total");
+    getElement.innerHTML = total;
+}
+function computeQty(e){
+    
+    var get_parent = e.closest("tr");
+    var get_price = get_parent.querySelector('.price');
+    if(get_price.value != "" || get_price.value != null || get_price.value != undefined){
+        var total = Number(e.value) * Number(get_price.value);
+        var get_total = get_parent.querySelector('.total');
+        get_total.value = total;
+        get_total.onchange();
 
+    }    
+}
+
+function computePrice(e){
+    var get_parent = e.closest("tr");
+    var get_qty = get_parent.querySelector('.qty');
+    console.log(get_qty);
+    if(get_qty.value != "" || get_qty.value != null || get_qty.value != undefined){
+        var total = Number(e.value) * Number(get_qty.value);
+        var get_total = get_parent.querySelector('.total');
+        get_total.value = total;
+        get_total.onchange();
+    }
+}
